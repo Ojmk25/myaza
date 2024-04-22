de0288423eecda4cca2e7116ca6ae1f43b448580
emmanuel.kalu <emmanuel.kalu@cecureintel.com>
2024-04-22 17:15:43 +0000

    fix: edit layout

--- web-content/app/layout.tsx
+++ web-content/app/layout.tsx
@@ -42,9 +42,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${metropolis_Font.variable} font-sans`}>
-        <MeetingProviderComponent>
           {children}
-        </MeetingProviderComponent>
       </body>
     </html>
   );
