d02bedd5ccfa8b4513314936212128d9d0fe73c0
EC2 Default User <ibimina.hart@cecureintel.com>
2025-01-28 08:20:56 +0000

    feat: add user guide page and link

--- web-content/components/Header.tsx
+++ web-content/components/Header.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import { useEffect, useRef, useState } from "react";
 import DateTimeDisplay from "@/utils/getDate";
-import { Profile, LoginCurve, MessageQuestion } from "iconsax-react";
+import { Profile, LoginCurve, MessageQuestion, InfoCircle } from "iconsax-react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import cecureStream from "@/public/assets/images/cecurestream.svg";
@@ -340,10 +340,18 @@ export default function Header() {
         </div>
       )}
       {showModal === "settings" && <Settings onClose={handleCloseModal} />}
-      <div
-        className=" w-fit fixed bottom-10 right-4 z-50 text-white"
+     <div
+        className=" w-fit fixed bottom-10 right-4 z-50 text-white flex items-center gap-2"
         ref={widgetRef}
       >
+           <div className="flex items-center gap-[6px] p-[10px]">
+              <InfoCircle
+                      size="16"
+                      color="#7133CF"
+                      className="mx-auto max-w-5"
+                    />
+                <p className="text-cs-purple-500 text-[14px]  lg:text-base leading-[18px] font-medium border-b-[1px] border-dashed border-cs-purple-500">User Guide</p>
+              </div>
         <div className="bg-cs-purple-650 text-cs-grey-60-light p-[10px] rounded-lg font-semibold flex items-center md:gap-x-2 cursor-pointer">
           <MessageQuestion size="20" color="#FAF0FF" className="m-auto" />
           <p className="hidden md:block">Feedback?</p>
